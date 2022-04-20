3cbbdc645400577291725c22afae63b0360c83d1
Ben <54992484+ben-maclaurin@users.noreply.github.com>
2022-04-20 18:44:50 +0100

    Create hammerspoon.lua

---
+++ hammerspoon.lua
@@ -0,0 +1,75 @@
+-- Window management 
+
+hs.hotkey.bind({"cmd", "shift"}, "H", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.h = max.h
+  f.w = max.w / 2
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "shift"}, "L", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + (max.w / 2)
+  f.h = max.h
+  f.w = max.w / 2
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "shift"}, "K", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.y = max.y
+  f.h = max.h / 2
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "shift"}, "J", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.y = max.y + (max.h / 2)
+  f.h = max.h / 2
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "shift"}, "Return", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x 
+  f.y = max.y
+  f.w = max.w
+  f.h = max.h 
+  win:setFrame(f)
+end)
+
+-- Deathadder compatibility with WebKit browsers
+
+overrideOtherMouseDown = hs.eventtap.new({ hs.eventtap.event.types.otherMouseDown }, function(e)
+    local mouseButton = e:getProperty(hs.eventtap.event.properties['mouseEventButtonNumber'])
+
+    if mouseButton == 4 then
+	hs.eventtap.keyStroke("cmd", "]")
+    elseif mouseButton == 3 then
+       hs.eventtap.keyStroke("cmd", "[")
+    end
+
+end)
+
+overrideOtherMouseDown:start()
